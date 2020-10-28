21eda6de0161f98a26e1022ba1751aeb93b02496
Kiran Gopalakrishnan <kgopalakrishnan@tempo.io>
2020-10-28 00:38:45 -0400

    Initial

---
+++ README.md
@@ -0,0 +1,2 @@
+# Recon
+ Recon is a type safe context powered state container providing a hook based API for React.
---
+++ src/recon.tsx
@@ -0,0 +1,65 @@
+import * as React from 'react'
+
+interface Action<T> {
+  type: T
+  payload: any
+}
+const createStore = <T, P>(
+  reducer: (state: T, action: Action<P>) => T,
+  initialState: T
+): [
+  React.FC<{ children: React.ReactNode; initial?: Partial<T> }>,
+  () => [T, ({ type, payload }: { type: P; payload: Partial<T> }) => any],
+  React.Context<null>
+] => {
+  const stateContext = React.createContext(null)
+
+  const reducerContext = React.createContext(null)
+
+  const useDispatch = (): (() => { type: P; payload: Partial<T> }) => {
+    const context: any = React.useContext(reducerContext)
+    if (context === undefined) {
+      throw new Error('dispatch must be used within a provider')
+    }
+    return context
+  }
+
+  const useStateValue = () => {
+    const context: any = React.useContext(stateContext)
+    if (context === undefined) {
+      throw new Error('state must be used within a provider')
+    }
+    return context
+  }
+
+  const Provider: React.FC<{ children: React.ReactNode; initial?: Partial<T> }> = ({
+    children,
+    initial
+  }: {
+    children: React.ReactNode
+    initial?: Partial<T>
+  }) => {
+    const initialValue:any = initial ? initial: initialState;
+    const [state, dispatch] = React.useReducer<(state: T, action: Action<P>) => T>(
+      reducer,
+      initialValue
+    )
+
+    return (
+      <stateContext.Provider value={state as any}>
+        <reducerContext.Provider value={dispatch as any}>{children}</reducerContext.Provider>
+      </stateContext.Provider>
+    )
+  }
+
+  const useState = (): [T, ({ type, payload }: { type: P; payload: Partial<T> }) => any] => [
+    useStateValue(),
+    useDispatch(),
+  ]
+  // stateContext should have been a private property ,
+  // but class components would require the actual context to access the values since they cannot use hooks
+  // TODO: stateContext should be private once all components are migrated to functional components
+  return [Provider, useState, stateContext]
+}
+
+export { createStore, Action }
